f9c68c5bbeb9400b21e674c9ef680b441629910d
Weaver <weevz@uw.edu>
2018-03-02 16:44:44 -0800

    initialize

--- audioqc.rb
+++ audioqc.rb
@@ -2,8 +2,11 @@
 
 require 'json'
 fileinput = ARGV[0]
-puts fileinput
+
 ffprobeout = JSON.parse(`ffprobe -print_format json -show_entries frame_tags=lavfi.astats.Overall.Peak_level -f lavfi -i "amovie='#{fileinput}',astats=metadata=1"`)
 ffprobeout['frames'].each_with_index do |metadata, index|
-	puts ffprobeout['frames'][index]['tags']['lavfi.astats.Overall.Peak_level']
+	peaklevel = ffprobeout['frames'][index]['tags']['lavfi.astats.Overall.Peak_level'].to_f
+	if peaklevel > -1.5
+		puts peaklevel
+	end
 end
