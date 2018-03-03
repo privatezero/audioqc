97c11c234daa34bf75516d4668e364ba3f7385f0
Weaver <weevz@uw.edu>
2018-03-02 16:48:40 -0800

    switch to method

--- audioqc.rb
+++ audioqc.rb
@@ -1,12 +1,17 @@
 #!/usr/local/bin/ruby
 
 require 'json'
-fileinput = ARGV[0]
 
-ffprobeout = JSON.parse(`ffprobe -print_format json -show_entries frame_tags=lavfi.astats.Overall.Peak_level -f lavfi -i "amovie='#{fileinput}',astats=metadata=1"`)
-ffprobeout['frames'].each_with_index do |metadata, index|
-	peaklevel = ffprobeout['frames'][index]['tags']['lavfi.astats.Overall.Peak_level'].to_f
-	if peaklevel > -1.5
-		puts peaklevel
-	end
+def CheckPeak(input)
+  ffprobeout = JSON.parse(`ffprobe -print_format json -show_entries frame_tags=lavfi.astats.Overall.Peak_level -f lavfi -i "amovie='#{input}',astats=metadata=1"`)
+  ffprobeout['frames'].each_with_index do |metadata, index|
+    peaklevel = ffprobeout['frames'][index]['tags']['lavfi.astats.Overall.Peak_level'].to_f
+    if peaklevel > -1.5
+      puts peaklevel
+    end
+  end
+end
+
+ARGV.each do |fileinput|
+  CheckPeak(fileinput)
 end
