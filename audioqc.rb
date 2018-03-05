a3b9b697ce5b5d6062525f827f9013feb3fcc5c0
Weaver <weevz@uw.edu>
2018-03-05 13:59:30 -0800

    start to add phase

--- audioqc.rb
+++ audioqc.rb
@@ -2,16 +2,20 @@
 
 require 'json'
 
-def CheckPeak(input)
-  ffprobeout = JSON.parse(`ffprobe -print_format json -show_entries frame_tags=lavfi.astats.Overall.Peak_level -f lavfi -i "amovie='#{input}',astats=metadata=1"`)
+def CheckAudioQuality(input)
+  $highdb = Array.new
+  ffprobeout = JSON.parse(`ffprobe -print_format json -show_entries frame_tags=lavfi.astats.Overall.Peak_level,lavfi.aphasemeter.phase -f lavfi -i "amovie='#{input}',astats=metadata=1,aphasemeter=video=0"`)
   ffprobeout['frames'].each_with_index do |metadata, index|
     peaklevel = ffprobeout['frames'][index]['tags']['lavfi.astats.Overall.Peak_level'].to_f
-    if peaklevel > -1.5
-      puts peaklevel
+    audiophase = ffprobeout['frames'][index]['tags']['lavfi.aphasemeter.phase'].to_f
+    if peaklevel > -5.5
+      $highdb << peaklevel
     end
+    puts audiophase
   end
+  puts $highdb.count
 end
 
 ARGV.each do |fileinput|
-  CheckPeak(fileinput)
+  CheckAudioQuality(fileinput)
 end
